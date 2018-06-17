aaec969e39b28e5577de0fc9e82ddf58aaf9f9e5
Alamgir Farouk <afarouk2@hotmail.com>
2018-06-17 02:02:14 -0700

    first

---
+++ includes/footer.php
@@ -0,0 +1,50 @@
+<footer id="footer"  class="dark_background">
+<!--
+	<section class="container footer-in">
+		<div class="col-md-3">
+			<div class="widget">
+				<div class="menu-footer1-container">
+					<ul class="menu">
+						<li>
+							<a>medicuris.com</a>
+						</li>
+
+						<li>
+							<a class="page-scroll" href=<?php echoActiveClassIfRequestMatches("index","contact") ?>>Contact</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</section>
+-->
+	<!-- end-footer-in -->
+	<section class="footbot">
+		<div class="container">
+			<div class="col-sm-3 footerlogo">
+				<a href="/"> <img id="footer-logo" src="images/logo1.png" alt="orinoco logo"></a>
+			</div>
+            <div class="col-md-4">
+                <div class="widget">
+                    <div class="menu-footer1-container">
+                        <ul class="footermenu">
+                            <li>
+                                <a>medicuris.com</a>
+                            </li>
+
+                            <li>
+                                <a class="page-scroll" href=<?php echoActiveClassIfRequestMatches("index","contact") ?>Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+			<div class="col-sm-5">
+                <div class="copyright">
+				    <a href="/">&#0169 &nbsp;2018. All rights reserved.&nbsp; | medicuris.com</a>
+                </div>
+			</div>
+		</div>
+	</section>
+	<!-- end-footbot -->
+</footer>
