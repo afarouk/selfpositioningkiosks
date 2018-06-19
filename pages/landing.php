0c45e5d91b091416cb4e2e513a89aba4021d5de0
Alamgir Farouk <afarouk2@hotmail.com>
2018-06-19 01:28:05 -0700

    update

--- pages/landing.php
+++ pages/landing.php
@@ -1,218 +1,225 @@
 <!-- section slider start -->
 <section id="slider-part">
- <div class="bg-overlay pattern"></div>
- <div class="container">
-  <div class="row">
-   <div class="col-md-offset-3 col-lg-offset-3 col-md-9 col-lg-9 col-sm-12 col-xs-12 wow fadeInLeft">
-    <div class="slider-content text-center">
-     <h2 class="slider-title">Self  Positioning<br> Kiosks</h2>
-     <h4 class="slider-title"> Machines adapting to people</h4>
-     <!--
-     <div class="btn-container">
-     <button class="btn btn-default featured">
-     Discover
-     </button>
-     <button class="btn btn-default">
-     Download Now
-     </button>
-     </div>
-     -->
+  <div class="bg-overlay pattern"></div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-offset-2 col-lg-offset-2 col-md-10 col-lg-10 col-sm-12 col-xs-12 wow fadeInLeft">
+        <div class="slider-content text-center">
+          <h2 class="slider-title">Self Positioning Kiosks</h2>
+          <h4 class="slider-title">Machines adapting to people</h4>
+          <!--
+            <div class="btn-container">
+            <button class="btn btn-default featured">
+            Discover
+            </button>
+            <button class="btn btn-default">
+            Download Now
+            </button>
+            </div>
+            -->
+        </div>
+      </div>
     </div>
-   </div>
-  </div><!-- row end -->
- </div>
- <!-- container end -->
+    <!-- row end -->
+  </div>
+  <!-- container end -->
 </section>
 <!-- section slider end -->
-
 <!-- Section 'what we do' start -->
 <section id="highlights" class="white_background">
- <div class="container">
-  <div class="row">
-   <div class="heading-inner text-center">
-    <h2 class="sec-title"> The future <span> is here </span></h2>
-    <h4 class="sec-sub-title text-center"> Soon you will be pulling up in your self-driving car, and a self-positioning kiosk will come up to you, to take your order </h4>
-   </div>
+  <div class="container">
+    <div class="row">
+      <div class="heading-inner text-center">
+        <h2 class="sec-title"> The future <span> is here </span></h2>
+        <h4 class="sec-sub-title text-center"> Soon you will be pulling up in your self-driving car, and a self-positioning kiosk will come up to you, to take your order </h4>
+      </div>
+    </div>
+    <!-- heading row end -->
+    <div class="row">
+      <div class="col-md-4 col-sm-6">
+        <div class="row">
+          <div class="col-md-12 wow slideInLeft" data-wow-delay=".3s">
+            <div class="about-inner ">
+              <img src="images/anonymous.png" />
+              <div class="about-content">
+                <h4>The problem </h4>
+                <p>We humans are diverse, we come in all shapes and sizes. Machines, on the other hand, are standardized. So, in man-machine-interfaces, it is the human who
+                  has to adapt to the machine. At least, that is how it is today. We want to change the relationship. We believe machines should adapt to us.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-12  wow slideInLeft" data-wow-delay=".5s">
+            <div class="about-inner ">
+              <img src="images/improved_monitoring.png"/>
+              <div class="about-content">
+                <h4>The solution</h4>
+                <p>
+                  The patient can answer questions, upload pictures, chat. Integrated IOT devices add physical measurements in the same secure and anonymised manner.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- row end -->
+      </div>
+      <div class="col-md-4 col-sm-4 col-xs-4 visible-md visible-lg wow fadeInDown" data-wow-delay=".3s">
+        <img src="images/app_lifecycle.png" alt="" class="img-responsive">
+      </div>
+      <div class="col-md-4 col-sm-6">
+        <div class="row">
+          <div class="col-md-12 wow slideInRight" data-wow-delay=".3s">
+            <div class="about-inner ">
+              <img src="images/nothing_to_download.png" />
+              <div class="about-content">
+                <h4>Accessibility</h4>
+                <p>
+                  No Appstore account needed, no login to remember, nothing to download. Millennials or otherwise, tech-savvy or not, all that is needed is a working smartphone.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-12 wow slideInRight" data-wow-delay=".5s">
+            <div class="about-inner ">
+              <img src="images/lower_cost.png" />
+              <div class="about-content">
+                <h4>Head-following</h4>
+                <p>
+                  There are many other applications of our core technology, of being able identify and follow a head of a person. Maybe it is a surgeon's assistant, or a helper in a factory.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- row end -->
+      </div>
+    </div>
+    <!-- row end -->
   </div>
-  <!-- heading row end -->
-  <div class="row">
-   <div class="col-md-4 col-sm-6">
-    <div class="row">
-     <div class="col-md-12 wow slideInLeft" data-wow-delay=".3s">
-      <div class="about-inner ">
-       <img src="images/anonymous.png" />
-       <div class="about-content">
-        <h4>Machines of the future</h4>
-        <p>We humans are diverse, we come in all shapes and sizes. Machines, on the other hand, are standardized. So, in man-machine-interfaces, it is the human who
-          has to adapt to the machine. At least, that is how it is today. We want to change the relationship. We believe machines should adapt to us. 
-        </p>
-       </div>
-      </div>
-     </div>
-    </div>
-    <!-- row end -->
-   </div>
-
-   <div class="col-md-4 col-sm-4 col-xs-4 visible-md visible-lg wow fadeInDown" data-wow-delay=".3s">
-    <img src="images/app_lifecycle.png" alt="" class="img-responsive">
-   </div>
-
-   <div class="col-md-4 col-sm-6">
-    <div class="row">
-     <div class="col-md-12 wow slideInRight" data-wow-delay=".3s">
-      <div class="about-inner ">
-       <img src="images/nothing_to_download.png" />
-       <div class="about-content">
-        <h4>Nothing to install</h4>
-        <p>
-         No Appstore account needed, no login to remember, nothing to download. Millennials or otherwise, tech-savvy or not, all that is needed is a working smartphone. </p>
-       </div>
-      </div>
-     </div>
-     <div class="col-md-12 wow slideInRight" data-wow-delay=".5s">
-      <div class="about-inner ">
-       <img src="images/lower_cost.png" />
-       <div class="about-content">
-        <h4>Reduced cost</h4>
-        <p>
-          Reduce unnecessary re-admissions by allowing patients to have follow up care via virtual follow-ups. Incrementally deployable, with little IT infrastructure required.        </p>
-       </div>
-      </div>
-     </div>
-    </div>
-    <!-- row end -->
-   </div>
-  </div><!-- row end -->
- </div><!-- container end -->
+  <!-- container end -->
 </section>
 <!-- Section features end -->
-
 <section id="scenario"  class="white_background">
- <div  class="container ">
-  <!-- FEATURETTE BEGIN  -->
-  <div class="row">
-   <div class="heading-inner text-center">
-    <h2 class="sec-title">A typical <span>scenario</span></h2>
-    <p>You just had surgery. Your doctor wants to monitor you closely.</p>
-   </div>
-
-      <div class="col-md-4">
-         <ul class="scenario_list">
-          <li>
-           The doctor/nurse creates your profile and sends a link to you, for your virtual follow-up.
-          </li>
-          <li>
-           You don't even have to login. No identifiable credentials are ever used in any communication.
-          </li>
-          <li>
-           Communicate, answer questionnaires, upload pictures, chat, ask questions securely.
-          </li>
-          <li>
-           Integrated IOT devices allow your doctor to monitor you in real-time, as he deems necessary.
-          </li>
-         </ul>
+  <div  class="container ">
+    <!-- FEATURETTE BEGIN  -->
+    <div class="row">
+      <div class="heading-inner text-center">
+        <h2 class="sec-title">A typical <span>scenario</span></h2>
+        <p>You just had surgery. Your doctor wants to monitor you closely.</p>
       </div>
-
-   <div class="col-md-8 featurette-image-frame">
-    <span class="featurette-image-helper"> <img src="images/projects/medicuris/curis_app.png"
-     class="featurette-image img-responsive" alt="500x400"></span>
-   </div>
+      <div class="col-md-4">
+        <ul class="scenario_list">
+          <li>
+            The doctor/nurse creates your profile and sends a link to you, for your virtual follow-up.
+          </li>
+          <li>
+            You don't even have to login. No identifiable credentials are ever used in any communication.
+          </li>
+          <li>
+            Communicate, answer questionnaires, upload pictures, chat, ask questions securely.
+          </li>
+          <li>
+            Integrated IOT devices allow your doctor to monitor you in real-time, as he deems necessary.
+          </li>
+        </ul>
+      </div>
+      <div class="col-md-8 featurette-image-frame">
+        <span class="featurette-image-helper"> <img src="images/projects/medicuris/curis_app.png"
+          class="featurette-image img-responsive" alt="500x400"></span>
+      </div>
+    </div>
+    <!-- FEATURETTE END  -->
+    <hr style="border: none;" />
+    <hr/>
+    <hr style="border: none;" />
   </div>
-  <!-- FEATURETTE END  -->
-  <hr style="border: none;" />
-  <hr/>
-  <hr style="border: none;" />
-
- </div>
 </section>
 <!-- section Contact start -->
 <section id="contact" >
- <div class="bg-overlay pattern"></div>
- <div class="container">
-  <div class="col-md-12">
-   <div class=" text-center">
-    <h2 class="sec-title">Contact <span>us</span></h2>
-    <div class="line-btm c-white"></div>
-    <p>
-     Questions, comments please let us know
-    </p>
-   </div>
-  </div>
-  <!-- heading row end -->
-
-  <div class="row" id="contactUsFormRow">
-   <div class="col-md-12">
-       <form class="contact-inner" id="contactUsForm" method="post" autocomplete="off">
-     <div class="row">
-      <div class="col-md-4">
-       <div class="form-group">
-        <label > Name</label>
-        <input type="text" class="form-control" id="name" name="senderName" placeholder="Your Name" required>
-       </div>
-          <div class="validationErrorMessageClass" style="color:red"></div>
+  <div class="bg-overlay pattern"></div>
+  <div class="container">
+    <div class="col-md-12">
+      <div class=" text-center">
+        <h2 class="sec-title">Contact <span>us</span></h2>
+        <div class="line-btm c-white"></div>
+        <p>
+          Questions, comments please let us know
+        </p>
       </div>
-      <div class="col-md-4">
-       <div class="form-group">
-        <label > Email</label>
-        <input type="text" class="form-control" id="email" name="replyToEmail" placeholder="Your Email" data-fv-emailaddress='true' data-fv-emailaddress-message='please enter valid email' required >
-       </div>
-          <div class="validationErrorMessageClass" style="color:red"></div>
-      </div>
-      <div class="col-md-4">
-       <div class="form-group">
-        <label > Subject</label>
-        <input type="text" class="form-control" id="subject" name="subject" placeholder="Subject" required>
-       </div>
-          <div class="validationErrorMessageClass" style="color:red"></div>
-      </div>
+    </div>
+    <!-- heading row end -->
+    <div class="row" id="contactUsFormRow">
       <div class="col-md-12">
-       <div class="form-group">
-        <label > Message</label>
-        <textarea id="message" name="description" cols="30" rows="8" class="form-control" placeholder="Message" required></textarea>
-       </div>
-          <div class="validationErrorMessageClass" style="color:red"></div>
+        <form class="contact-inner" id="contactUsForm" method="post" autocomplete="off">
+          <div class="row">
+            <div class="col-md-4">
+              <div class="form-group">
+                <label > Name</label>
+                <input type="text" class="form-control" id="name" name="senderName" placeholder="Your Name" required>
+              </div>
+              <div class="validationErrorMessageClass" style="color:red"></div>
+            </div>
+            <div class="col-md-4">
+              <div class="form-group">
+                <label > Email</label>
+                <input type="text" class="form-control" id="email" name="replyToEmail" placeholder="Your Email" data-fv-emailaddress='true' data-fv-emailaddress-message='please enter valid email' required >
+              </div>
+              <div class="validationErrorMessageClass" style="color:red"></div>
+            </div>
+            <div class="col-md-4">
+              <div class="form-group">
+                <label > Subject</label>
+                <input type="text" class="form-control" id="subject" name="subject" placeholder="Subject" required>
+              </div>
+              <div class="validationErrorMessageClass" style="color:red"></div>
+            </div>
+            <div class="col-md-12">
+              <div class="form-group">
+                <label > Message</label>
+                <textarea id="message" name="description" cols="30" rows="8" class="form-control" placeholder="Message" required></textarea>
+              </div>
+              <div class="validationErrorMessageClass" style="color:red"></div>
+            </div>
+          </div>
+          <!-- row end -->
+          <div class="row">
+            <div class="col-md-12 text-center">
+              <button class="btn btn-default" id="contactSubmit" >
+              Submit
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
-     </div>
-     <!-- row end -->
-     <div class="row">
-      <div class="col-md-12 text-center">
-       <button class="btn btn-default" id="contactSubmit" >
-        Submit
-       </button>
-      </div>
-     </div>
-    </form>
-   </div>
-  </div><!-- row end -->
-  <div class="row" id="contactUsSuccessRow">
-    <span class="glyphicon glyphicon-ok icon-size" aria-hidden="true"></span>
-    <h1 id='contactUsSuccessDiv'></h1>
+    </div>
+    <!-- row end -->
+    <div class="row" id="contactUsSuccessRow">
+      <span class="glyphicon glyphicon-ok icon-size" aria-hidden="true"></span>
+      <h1 id='contactUsSuccessDiv'></h1>
+    </div>
+    <div class="row" id="contactUsErrorRow">
+      <!-- <span class="glyphicon glyphicon-remove icon-size" aria-hidden="true"></span> -->
+      <h1 id='contactUsErrorDiv'></h1>
+    </div>
   </div>
-  <div class="row" id="contactUsErrorRow">
-    <!-- <span class="glyphicon glyphicon-remove icon-size" aria-hidden="true"></span> -->
-    <h1 id='contactUsErrorDiv'></h1>
-  </div>
- </div><!-- container end -->
+  <!-- container end -->
 </section>
 <!-- section Contact end -->
-
 <!-- Back To Top Button -->
 <div id="back-top">
- <a href="#slider-part" class="page-scroll btn btn-primary" ><i class="fa fa-angle-double-up"></i></a>
+  <a href="#slider-part" class="page-scroll btn btn-primary" ><i class="fa fa-angle-double-up"></i></a>
 </div>
 <!-- End Back To Top Button -->
-
 <script>new WOW().init();</script>
 <script>
-$.backstretch([ "images/bg/bg1.jpg", "images/bg/bg2.jpg",
-    "images/bg/bg3.jpg","images/bg/bg4.jpg" ,"images/bg/bg5.jpg" ,"images/bg/bg6.jpg" ,"images/bg/bg7.jpg","images/bg/bg8.jpg"  ], {
-  fade : 850,
-  duration : 3000
-});
+  $.backstretch([ "images/bg/bg1.png"  ], {
+    fade : 850,
+    duration : 3000
+  });
 </script>
 <script>
-	$('.counter').counterUp({
-		delay : 100,
-		time : 2000
-	});
+  $('.counter').counterUp({
+  	delay : 100,
+  	time : 2000
+  });
 </script>
